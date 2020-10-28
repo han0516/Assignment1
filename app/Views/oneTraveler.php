14b9ada53449de66d44c7aecb2388a20af33ebf5
HANG <HANG@192.168.56.1>
2020-10-28 21:56:48 +0800

    Initial commit

--- app/Views/oneTraveler.php
+++ app/Views/oneTraveler.php
@@ -6,8 +6,9 @@
 	<body>
 		<h1>One Traveler</h1>
 		<p><strong>ID:</strong> {id}</p>
-		<p><strong>Name:</strong> {name}</p>
+		<p><strong>Name:</strong> {name}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<strong>Chinesename:</strong> {Chinesename}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<strong>weapon:</strong> {weapon}&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<strong>position:</strong> {position}</p>
                 <p>{do}</p>
+                <p><strong>description:</strong> {description}</p>
 		<p><img src="/image/{image}"/></p>
 		<p><a href="/home">Home</a></p>
 	</body>
